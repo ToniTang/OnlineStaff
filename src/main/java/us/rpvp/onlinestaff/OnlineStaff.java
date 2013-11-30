1311bc2b1e08c4804187f38f45d3d57710c9cf19
ToniTang <anthony.n.tang@gmail.com>
2013-11-30 03:54:43 -0600

    WORKING STAGE!!! First plugin, almost check!
    After 5-6 total hours of working I've finally managed to get a working
    build that not only compiles, but can actually create, update, and
    manage a database! Now what's left is a little code tidying, changing of
    strings, and hopefully any optimization methods. But that's all for
    tomorrow.

--- src/main/java/us/rpvp/onlinestaff/OnlineStaff.java
+++ src/main/java/us/rpvp/onlinestaff/OnlineStaff.java
@@ -1,6 +1,7 @@
 package us.rpvp.onlinestaff;
 
 import java.lang.String;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -14,22 +15,24 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
-import sun.jdbc.odbc.ee.ConnectionHandler;
 
 public class OnlineStaff extends JavaPlugin implements Listener {
 
-    Connection con;
+    public Connection con;
 
     public void onEnable() {
         getLogger().info("===========================================");
         getLogger().info(String.format("[v%s] OnlineStaff has been enabled.", getDescription().getVersion()));
         getLogger().info("===========================================");
         getServer().getPluginManager().registerEvents(this, this);
+
         this.saveDefaultConfig();
+
         String hostname = getConfig().getString("mysql.hostname");
         String username = getConfig().getString("mysql.username");
         String password = getConfig().getString("mysql.password");
         String database = getConfig().getString("mysql.database");
+
         startConnection(hostname, username, password, database);
     }
 
@@ -37,35 +40,54 @@ public class OnlineStaff extends JavaPlugin implements Listener {
         getLogger().info("===========================================");
         getLogger().info(String.format("[v%s] OnlineStaff has been disabled.", getDescription().getVersion()));
         getLogger().info("===========================================");
+        closeConnection();
     }
 
-    public boolean startConnection(String hostname, String username, String password, String database) {
+    public void startConnection(String hostname, String username, String password, String database) {
         Statement str;
         try {
             Class.forName("com.mysql.jdbc.Driver").newInstance();
             con = DriverManager.getConnection("jdbc:mysql://" + hostname + ":3306/" + database, username, password);
+
             str = con.createStatement();
             String query = "CREATE TABLE IF NOT EXISTS `OnlineStaff` ("
                     + "  `player` varchar(16) NOT NULL,"
-                    + "  `last_online` int(16) NOT NULL,"
-                    + "  `is_online` tinyint(1) NOT NULL"
+                    + "  `last_online` varchar(16) NOT NULL,"
+                    + "  `is_online` tinyint(1) NOT NULL,"
+                    + "  UNIQUE KEY `player` (`player`)"
                     + ") ENGINE=InnoDB DEFAULT CHARSET=latin1;";
             str.executeUpdate(query);
             str.close();
         }
         catch (Exception e) {
-            System.out.println("[OnlineStaff] Failed to connect to database");
             e.printStackTrace();
-            return false;
         }
-        return true;
+    }
+
+    public void closeConnection() {
+        try {
+            this.con.close();
+        }
+        catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean onCommand(CommandSender sender, Command cmd, String Label, String[] args) {
         if(sender.hasPermission("onlinestaff.reload")) {
             if(cmd.getLabel().equalsIgnoreCase("osreload")) {
                 sender.sendMessage(ChatColor.AQUA + "[OnlineStaff]" + ChatColor.GOLD + " Configuration reloaded.");
+
+                closeConnection();
+
                 this.reloadConfig();
+
+                String hostname = getConfig().getString("mysql.hostname");
+                String username = getConfig().getString("mysql.username");
+                String password = getConfig().getString("mysql.password");
+                String database = getConfig().getString("mysql.database");
+
+                startConnection(hostname, username, password, database);
             }
         }
         else {
@@ -75,40 +97,16 @@ public class OnlineStaff extends JavaPlugin implements Listener {
     }
 
     @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        /********************************************
-        Method / Plan here is that when the user joins
-        and triggers the PlayerJoinEvent, a SQL statement
-        will be formatted, escaped, and then queried into
-        the database updating the player's row.
-            Case 1:
-            - User is joining for the first time
-                - Create a manager or check method that
-                  will run and see if the player's row
-                  exists based off of the player's name
-                - If it doesn't exist, append the player's
-                  name into a already predefined statement
-                  and query it into the database.
-                - Field (isOnline) = 1 ((true))
-            Case 2:
-            - User row exists and is returning to server
-                - Use predefined query statement and append
-                  player name to it and update the field
-                  isOnline and set to (1) true
-        ********************************************/
+    public void onPlayerJoin(PlayerJoinEvent event) throws SQLException {
+        Statement str = this.con.createStatement();
+        String query = String.format("INSERT INTO `OnlineStaff` (player, last_online, is_online) VALUES ('%s', 'Currently Online', 1) ON DUPLICATE KEY UPDATE last_online = 'Currently Online', is_online = '1'", event.getPlayer().getName());
+        str.executeUpdate(query);
     }
 
     @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent event) {
-        /********************************************
-         Method / Plan here is that when the user quits
-         and triggers the PlayerQuitEvent, a SQL statement
-         will be formatted, escaped, and then queried into
-         the database updating the player's row.
-            Case 1:
-            - Use predefined query statement and append
-              player name to it and update the field
-              isOnline and set it to (0) false
-         ********************************************/
+    public void onPlayerQuit(PlayerQuitEvent event) throws SQLException {
+        Statement str = this.con.createStatement();
+        String query = String.format("UPDATE `OnlineStaff` SET `last_online` = 'Just logged', `is_online`  = '0' WHERE player = '%s'", event.getPlayer().getName());
+        str.executeUpdate(query);
     }
 }
